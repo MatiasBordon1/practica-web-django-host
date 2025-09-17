52bf56633418910f06ebb8ec3bf205ee66f594f5
Matias Bordon <matzaia2001@gmail.com>
2025-09-16 22:50:24 -0300

    psw

--- miniportal/settings.py
+++ miniportal/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY", "cambiame-en-desarrollo")
+SECRET_KEY = os.getenv("SECRET_KEY", "miniportal1234")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
